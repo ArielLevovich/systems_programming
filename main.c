9a77bafb1453c4ea01c71be8453f68ceccc7cb9d
MottyLevovich <ariel.levovich@msmail.ariel.ac.il>
2024-01-23 22:50:00 +0200

    a1bbf7c

--- main.c
+++ main.c
@@ -12,29 +12,29 @@ int main() {
     int num1, num2;
     scanf("%d %d", &num1, &num2); // Read two integers
 
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = num1; i <= num2; i++) {
         if (isArmstrong(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Palindromes are: ");
+    printf("\nThe Palindromes are:");
     for (int i = num1; i <= num2; i++) {
         if (isPalindrome(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for (int i = num1; i <= num2; i++) {
         if (isPrime(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
 
-    printf("\nThe Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for (int i = num1; i <= num2; i++) {
         if (isStrong(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
